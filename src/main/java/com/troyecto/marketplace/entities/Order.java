26942d8c84c26e0dc84e0671c3e96b670c9a9ef7
Humberto <humbertoescobarmoreno@gmail.com>
2025-10-09 13:40:07 -0400

    Atributos de tabla Order

--- src/main/java/com/troyecto/marketplace/entities/Order.java
+++ src/main/java/com/troyecto/marketplace/entities/Order.java
@@ -17,15 +17,26 @@ public class Order {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String orderNumber;
+    @Column(nullable = false)
     private String buyer;   //Cambiar despues a ManytoOne con user
+    @Column(nullable = false)
     private String seller;  //Cambiar despues a ManytoOne con user
+    @Column(nullable = false)
     private Long items;     //Cambiar despues a List<orderItems>
+    @Column(nullable = false)
     private BigDecimal subtotal;
+    @Column(nullable = false)
     private BigDecimal totalAmount;
+    @Column(nullable = false)
     private BigDecimal tax;
+    @Column(nullable = false)
     private String currency;
+    @Column(nullable = false)
     private String payMethod;
+    @Column(nullable = false)
     private String paymentStatus;
+    @Column(nullable = false)
     private LocalDateTime orderDate;
 }
